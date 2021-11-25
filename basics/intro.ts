44458bca2b6a57898267bd87b3df3a8212094224
dev-arminder <mail.to.arminder.singh@gmail.com>
2021-11-25 23:55:56 +0530

    Hands Dirty with TS

--- basics/intro.ts
+++ basics/intro.ts
@@ -1,1 +1,20 @@
 // The main benefit of TS is that it can highlight unexpected behavior in your JS code and lowering the chance of bugs.
+let helloworld = "Hello World";
+
+// Defining types
+// interface declaration
+interface User {
+  name: String; // ENDS WITH SEMICOLON
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("Murphy", 1);
