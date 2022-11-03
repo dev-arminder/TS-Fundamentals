d51f7b3e5c0d71f90ec2bf641b960d4a345a4c20
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-11-04 00:20:20 +0530

    Function Annonation in TS

--- Basics/002.ts
+++ Basics/002.ts
@@ -1,3 +1,20 @@
 /* 
  * TYPE ANNOTATIONS
+ * Primitives Types
  */
+
+let userName: string = 'Arminder';
+let age: number = 41;
+let isSinger: boolean = false;
+
+let isFamous: boolean;
+
+// Error If Trying to use variable without any value
+// console.log(isFamous)
+
+/* 
+ * Array
+*/
+
+let users: string[] = ['Arminder', 'Singh', 'Gukki'];
+let score: Array<number | string> = [1, 2] 
