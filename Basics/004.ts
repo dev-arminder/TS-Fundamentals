630247b54069f5b86a87858dab8b2df5e1d1b341
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-11-05 21:56:01 +0530

    Function Type annotation

---
+++ Basics/004.ts
@@ -0,0 +1,18 @@
+/* 
+  * Function and Types
+  * Parameters Annotation 
+  * Return value annotation
+ */
+
+function greet(name: string){
+  console.log(`Hi Thanks ${name} for visiting !!`)
+}
+
+greet("Arminder Singh")
+
+// Return type annotationn
+function randomNumber(): number{
+  return Math.random()
+}
+
+randomNumber()
---
+++ Basics/005.ts
@@ -0,0 +1,10 @@
+/* 
+ * Contextual Inferencing
+ */
+
+let arr = ["Arminder", "randeep", "simran", "ritik", 54]
+
+// name infer type string
+arr.forEach(function(name){
+  console.log('Name is of Type stirng.')
+})
